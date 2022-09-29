c690e940e1211c3c0515a0fc8303073f893d8f60
hadh93 <hoondog1993@gmail.com>
2022-09-28 21:43:45 -0500

    Test for HelloWorldController implemented using MockMvc

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/controller/HelloWorldController.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.fastcampus.javaallinone.project3.mycontact.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // => @ResponsiveBody + @Controller
