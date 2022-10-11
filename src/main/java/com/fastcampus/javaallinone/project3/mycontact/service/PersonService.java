e8db5a31bca1a7bdfa47658a786a34e7df525b95
hadh93 <hoondog1993@gmail.com>
2022-10-10 22:26:03 -0500

    JPA query method findby: bloodtype, birthday between implemented

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -22,11 +22,14 @@ public class PersonService {
     private BlockRepository blockRepository;
 
     public List<Person> getPeopleExcludeBlocks(){
-        List<Person> people = personRepository.findAll();
-//        List<Block> blocks = blockRepository.findAll();
 
+//        List<Block> blocks = blockRepository.findAll();
 //        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
+        /*
+        List<Person> people = personRepository.findAll();
         return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
+         */
+        return personRepository.findByBlockIsNull();
     }
 
     @Transactional
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -51,7 +51,6 @@ class PersonRepositoryTest {
 
 
 
-
     @Test
     void hashCodeAndEquals(){
         Person person1 = new Person("martin", 10, "A");
@@ -67,4 +66,41 @@ class PersonRepositoryTest {
 
     }
 
+
+    @Test
+    void findByBloodType(){
+        givenPerson("martin", 10, "A");
+        givenPerson("david", 9, "B");
+        givenPerson("dennis", 8, "O");
+        givenPerson("sophia", 7, "AB");
+        givenPerson("benny", 6, "A");
+
+
+        List<Person> result = personRepository.findByBloodType("A");
+        result.forEach(System.out::println);
+
+    }
+
+    @Test
+    void findByBirthdayBetween(){
+        givenPerson("martin", 10, "A", LocalDate.of(1991,8,15));
+        givenPerson("david", 9, "B", LocalDate.of(1992,7,10));
+        givenPerson("dennis", 8, "O", LocalDate.of(1993,1,5));
+        givenPerson("sophia", 7, "AB", LocalDate.of(1994,6,30));
+        givenPerson("benny", 6, "A", LocalDate.of(1995,8,30));
+
+        List<Person> result = personRepository.findByBirthdayBetween(LocalDate.of(1991, 8, 1), LocalDate.of(1991, 8, 31));
+        result.forEach(System.out::println);
+    }
+
+    private void givenPerson(String name, int age, String bloodType){
+        givenPerson(name, age, bloodType, null);
+    }
+
+    private void givenPerson(String name, int age, String bloodType, LocalDate birthday){
+        Person person = new Person(name, age, bloodType);
+        person.setBirthday(birthday);
+        personRepository.save(person);
+
+    }
 }
