25a8fd45980db01b2ee5d370dc16f76832a8e4f7
hadh93 <hoondog1993@gmail.com>
2022-10-13 18:30:51 -0500

    Bug in data.sql found and need to be fixed

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,12 @@
 insert into person('id', 'name', 'age', 'blood_type') values (1, 'martin', 10, 'A');
+-- TODO: fix following bug in this sql file:
+-- org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'dataSourceScriptDatabaseInitializer'
+-- defined in class path resource [org/springframework/boot/autoconfigure/sql/init/DataSourceInitializationConfiguration.class]:
+--
+-- Invocation of init method failed; nested exception is
+-- org.springframework.jdbc.datasource.init.ScriptStatementFailedException:
+-- Failed to execute SQL script statement #1 of URL [file:/D:/FAST_JAVA/demo/mycontact/build/resources/test/data.sql]:
+-- insert into person('id', 'name', 'age', 'blood_type') values (1, 'martin', 10, 'A');
+--
+-- nested exception is org.h2.jdbc.JdbcSQLSyntaxErrorException:
+-- Table "PERSON" not found (this database is empty); SQL statement:
